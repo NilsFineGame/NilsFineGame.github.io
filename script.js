6cba25578575c35ae9bb6eef04a2088380f6f1c1
NilsFineGame <dizzyawe12@gmail.com>
2024-06-15 18:18:28 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function changeImage(x,image,y)
+{
+
+    if (x===1)
+    {
+        image.src = "images/prem2.png"
+    }
+    if (x===2)
+    {
+        image.src = "images/prem1.png"
+    }
+    if (x===3)
+        {
+        image.src = "images/dem1.png"
+        }
+    if (x===4)
+        {
+        image.src = "images/dem2.png"
+        }
+    if (x===5)
+        {
+        image.src = "images/cred2.png"
+        }
+    if (x===6)
+        {
+        image.src = "images/cred1.png"
+        }
+    if (x===7)
+        {
+        image.src = "images/disc2.png"
+        }
+    if (x===8)
+        {
+        image.src = "images/disc1.png"
+        }
+
+
+}
