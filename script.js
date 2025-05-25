3bd0dd9c5cdcffaedbc3aaa77ddc2838dc97d7e2
DizzyAwe <66225003+dizzyawe@users.noreply.github.com>
2025-05-25 00:03:09 -0700

    path fix
    q

--- script.js
+++ script.js
@@ -3,35 +3,35 @@ function changeImage(x,image,y)
 
     if (x===1)
     {
-        image.src = "images/prem2.png"
+        image.src = "/images/prem2.png"
     }
     if (x===2)
     {
-        image.src = "images/prem1.png"
+        image.src = "/images/prem1.png"
     }
     if (x===3)
         {
-        image.src = "images/dem1.png"
+        image.src = "/images/dem1.png"
         }
     if (x===4)
         {
-        image.src = "images/dem2.png"
+        image.src = "/images/dem2.png"
         }
     if (x===5)
         {
-        image.src = "images/cred2.png"
+        image.src = "/images/cred2.png"
         }
     if (x===6)
         {
-        image.src = "images/cred1.png"
+        image.src = "/images/cred1.png"
         }
     if (x===7)
         {
-        image.src = "images/disc2.png"
+        image.src = "/images/disc2.png"
         }
     if (x===8)
         {
-        image.src = "images/disc1.png"
+        image.src = "/images/disc1.png"
         }
 
 
