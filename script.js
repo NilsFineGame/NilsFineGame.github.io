30f64dd82952eded59d3fd499d812dabe7dbdada
DizzyAwe <66225003+dizzyawe@users.noreply.github.com>
2025-05-25 21:33:12 -0700

    dfdfs
    d

--- script.js
+++ script.js
@@ -28,35 +28,35 @@ function changeImage(x,image,y)
 
     if (x===1)
     {
-        image.src = "/images/prem2.png"
+        image.src = "/images/ui/spr_devlog_2.png"
     }
     if (x===2)
     {
-        image.src = "/images/prem1.png"
+        image.src = "/images/ui/spr_devlog_1.png"
     }
     if (x===3)
         {
-        image.src = "/images/dem1.png"
+        image.src = "/images/ui/spr_demo_1.png"
         }
     if (x===4)
         {
-        image.src = "/images/dem2.png"
+        image.src = "/images/ui/spr_demo_2.png"
         }
     if (x===5)
         {
-        image.src = "/images/cred2.png"
+        image.src = "/images/ui/spr_credits_2.png"
         }
     if (x===6)
         {
-        image.src = "/images/cred1.png"
+        image.src = "/images/ui/spr_credits_1.png"
         }
     if (x===7)
         {
-        image.src = "/images/disc2.png"
+        image.src = "/images/ui/spr_discord_2.png"
         }
     if (x===8)
         {
-        image.src = "/images/disc1.png"
+        image.src = "/images/ui/spr_discord_1.png"
         }
 }
 function setBackground(bgKey) {
